0efa4adf45ea137bb2fe0b85a7c6c958a1237642
Vaishnavi_gitHub29 <86351334+vaish29github@users.noreply.github.com>
2025-04-18 18:54:07 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -67,7 +67,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = var.node_count
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "Standard_B2ms"
   }
 
   identity {
